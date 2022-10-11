60fb8d0472a5a15b22764698019a41035368cf70
Juan Sanchez <juasanchez@bpd.com.do>
2022-10-11 09:25:02 -0400

    fix: Singleton patern on class task

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -1,18 +1,18 @@
 import { TaskModel } from "../models/task.js"
 export default class Task {
   public static async create(data: any): Promise<any> {
-    return new TaskModel().create(data)
+    return TaskModel.getInstance().create(data)
   }
   public static async update(data: any): Promise<any> {
-    return new TaskModel().update(data)
+    return TaskModel.getInstance().update(data)
   }
   public static async getById(id: string): Promise<any> {
-    return new TaskModel().getById(id)
+    return TaskModel.getInstance().getById(id)
   }
   public static async getAll(): Promise<[]> {
-    return new TaskModel().getAll()
+    return TaskModel.getInstance().getAll()
   }
   public static async delete(id: string): Promise<void> {
-    return new TaskModel().delete(id)
+    return TaskModel.getInstance().delete(id)
   }
 }
--- src/models/task.ts
+++ src/models/task.ts
@@ -2,23 +2,29 @@ import { Model } from "mongoose"
 import Mongo from "../database/mongo.js"
 
 export class TaskModel {
-  static model: Model<any>
-  constructor() {
-    if (!TaskModel.model) {
-      TaskModel.model = Mongo.db.TASK_MANAGER.model(
-        "task",
-        new Mongo.db.TASK_MANAGER.Schema({
-          name: { type: String, required: true },
-          status: { type: String, required: true, default: "pending" },
-          createdBy: { type: String, required: true },
-          date: { type: Date, default: Date.now },
-        })
-      )
-    }
+  private static instance: TaskModel
+  private model: Model<any>
+
+  public static getInstance(): TaskModel {
+    if (!TaskModel.instance) TaskModel.instance = new TaskModel()
+    return TaskModel.instance
   }
+
+  constructor() {
+    this.model = Mongo.db.TASK_MANAGER.model(
+      "task",
+      new Mongo.db.TASK_MANAGER.Schema({
+        name: { type: String, required: true },
+        status: { type: String, required: true, default: "pending" },
+        createdBy: { type: String, required: true },
+        date: { type: Date, default: Date.now },
+      })
+    )
+  }
+
   public async create(data: any): Promise<any> {
     try {
-      const task = new TaskModel.model(data)
+      const task = new this.model(data)
       const result = await task.save()
       return Promise.resolve({
         id: result.id,
@@ -30,15 +36,12 @@ export class TaskModel {
       return Promise.reject(error)
     }
   }
+
   public async update(data: any): Promise<any> {
     try {
-      const result: any = await TaskModel.model.findByIdAndUpdate(
-        data.id,
-        data,
-        {
-          new: true,
-        }
-      )
+      const result: any = await this.model.findByIdAndUpdate(data.id, data, {
+        new: true,
+      })
       return Promise.resolve({
         id: result.id,
         name: result.name,
@@ -49,9 +52,10 @@ export class TaskModel {
       return Promise.reject(error)
     }
   }
+
   public async getById(id: string): Promise<any> {
     try {
-      const result: any = await TaskModel.model.findById(id)
+      const result: any = await this.model.findById(id)
       return Promise.resolve({
         id: result.id,
         name: result.name,
@@ -62,9 +66,10 @@ export class TaskModel {
       return Promise.reject(error)
     }
   }
+
   public async getAll(): Promise<[]> {
     try {
-      const result: any = await TaskModel.model.find()
+      const result: any = await this.model.find()
       return Promise.resolve(
         result.map((t: any) => {
           return { id: t.id, name: t.name, createdBy: t.createdBy }
@@ -75,9 +80,10 @@ export class TaskModel {
       return Promise.reject(error)
     }
   }
+
   public async delete(id: string): Promise<void> {
     try {
-      const result: any = await TaskModel.model.findByIdAndDelete(id)
+      const result: any = await this.model.findByIdAndDelete(id)
       return Promise.resolve()
     } catch (error) {
       if (process.env.VERBOSE === "true") console.error(error)
