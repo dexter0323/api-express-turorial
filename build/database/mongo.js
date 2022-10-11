3d62fe65376c6db307b7d36d5c254cafbf2a9f07
dexter0323 <14128083+dexter0323@users.noreply.github.com>
2022-10-10 22:07:47 -0400

    fix: Database singlenton

--- build/database/mongo.js
+++ build/database/mongo.js
@@ -3,11 +3,11 @@ export default class Mongo {
     static db = [];
     static async connect(mongoUri, dbName) {
         try {
-            if (Mongo.db[dbName])
-                Promise.resolve(Mongo.db[dbName]);
-            Mongo.db[dbName] = await connect(mongoUri + dbName);
-            if (process.env.VERBOSE === "true")
-                console.info(`Connected to mongo db ${dbName}`);
+            if (!Mongo.db[dbName]) {
+                Mongo.db[dbName] = await connect(mongoUri + dbName);
+                if (process.env.VERBOSE === "true")
+                    console.info(`Connected to mongodb ${dbName}`);
+            }
             Promise.resolve(Mongo.db[dbName]);
         }
         catch (error) {
--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -3,10 +3,11 @@ export default class Mongo {
   public static db: any = []
   public static async connect(mongoUri: string, dbName: string) {
     try {
-      if (Mongo.db[dbName]) Promise.resolve(Mongo.db[dbName])
-      Mongo.db[dbName] = await connect(mongoUri + dbName)
-      if (process.env.VERBOSE === "true")
-        console.info(`Connected to mongo db ${dbName}`)
+      if (!Mongo.db[dbName]) {
+        Mongo.db[dbName] = await connect(mongoUri + dbName)
+        if (process.env.VERBOSE === "true")
+          console.info(`Connected to mongodb ${dbName}`)
+      }
       Promise.resolve(Mongo.db[dbName])
     } catch (error) {
       if (process.env.VERBOSE === "true") console.error(error)
